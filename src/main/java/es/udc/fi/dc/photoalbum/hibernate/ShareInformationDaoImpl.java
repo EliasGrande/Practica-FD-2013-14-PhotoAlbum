dfda9920d2256cd800cad63b54c3b93ce61eda28
EliasGrande <elias.grande.casedas@gmail.com>
2013-10-06 21:00:53 +0200

    Comentado ShareInformationDaoImpl, mas que nada para darnos cuenta de donde se usa, y para qué, cada método, cosa que no estaba muy clara antes (Igual deberíamos comentarlo en la interfaz y no aquí pero bueno, ahí queda por el momento).
    Modificado el ShareInformationDaoImpl.getShares(User,User) para filtrar
    los albumes marcados como privados por el propietario y que no se le
    muestren a otra persona aunque ésta aparezca en la lista de compartidos
    del album.

--- src/main/java/es/udc/fi/dc/photoalbum/hibernate/ShareInformationDaoImpl.java
+++ src/main/java/es/udc/fi/dc/photoalbum/hibernate/ShareInformationDaoImpl.java
@@ -13,17 +13,38 @@ import java.util.List;
 
 public class ShareInformationDaoImpl extends HibernateDaoSupport implements
 		ShareInformationDao {
+	
+//	private void debug(String method) {
+//		System.err.println("Debug: ShareInformationDaoImp[instance="+this.hashCode()+"]." + method);
+//	}
 
+	/**
+	 * Guarda un ShareInformation en base de datos.
+	 */
 	public void create(ShareInformation shareInformation) {
 		getHibernateTemplate().save(shareInformation);
 	}
 
+	/**
+	 * Elimina un ShareInformation de base de datos.
+	 */
 	public void delete(ShareInformation shareInformation) {
 		getHibernateTemplate().delete(shareInformation);
 	}
 
+	/**
+	 * ShareInformation de los albumes compartidos por un usuario a otro. Los
+	 * albumes privados son filtrados y no aparecen en el resultado. Usado en
+	 * "Albumes compartidos conmigo > usuario@ejemplo.com".
+	 * 
+	 * @param userShared
+	 *            Propietario
+	 * @param userSharedTo
+	 *            Otro usuario
+	 */
 	@SuppressWarnings("unchecked")
 	public List<ShareInformation> getShares(User userShared, User userSharedTo) {
+//		debug("getShares(User userShared, User userSharedTo)");
 		return (List<ShareInformation>) getHibernateTemplate().findByCriteria(
 				DetachedCriteria
 						.forClass(ShareInformation.class)
@@ -32,18 +53,31 @@ public class ShareInformationDaoImpl extends HibernateDaoSupport implements
 						.createAlias("user", "us")
 						.add(Restrictions.eq("alus.id", userShared.getId()))
 						.add(Restrictions.eq("us.id", userSharedTo.getId()))
-//						.add(Restrictions.or(Restrictions.like(
-//								"album.privacity",
-//								PrivacyLevel.SHAREABLE.toString(),
-//								MatchMode.EXACT),
-//								Restrictions.like("album.privacity",
-//										PrivacyLevel.PUBLIC.toString(),
-//										MatchMode.EXACT)))
+						.add(Restrictions
+								.disjunction()
+								.add(Restrictions
+										.like("al.privacyLevel",
+												PrivacyLevel.SHAREABLE,
+												MatchMode.EXACT))
+								.add(Restrictions.like("al.privacyLevel",
+										PrivacyLevel.PUBLIC, MatchMode.EXACT)))
 						.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY));
 	}
 
+	/**
+	 * ShareInformation de un album concreto. Usado en
+	 * "Albumes compartidos conmigo > usuario@ejemplo.com > AlbumEjemplo".
+	 * 
+	 * @param albumName
+	 *            Nombre del album
+	 * @param userSharedToId
+	 *            Id de usuario con el que se ha compartido
+	 * @param userSharedEmail
+	 *            Email del propietario
+	 */
 	public ShareInformation getShare(String albumName, int userSharedToId,
 			String userSharedEmail) {
+//		debug("getShare(String albumName, int userSharedToId, String userSharedEmail)");
 		@SuppressWarnings("unchecked")
 		ArrayList<ShareInformation> list = (ArrayList<ShareInformation>) getHibernateTemplate()
 				.findByCriteria(
@@ -65,8 +99,16 @@ public class ShareInformationDaoImpl extends HibernateDaoSupport implements
 		}
 	}
 
+	/**
+	 * Usado en "Mis albumes > AlbumEjemplo > Compartir" para mostrar la lista
+	 * de usuarios con los que he compartido el album.
+	 * 
+	 * @param albumId
+	 *            Identificador del album.
+	 */
 	@SuppressWarnings("unchecked")
 	public ArrayList<ShareInformation> getAlbumShares(int albumId) {
+//		debug("getAlbumShares(int albumId)");
 		return (ArrayList<ShareInformation>) getHibernateTemplate()
 				.findByCriteria(
 						DetachedCriteria
@@ -77,8 +119,16 @@ public class ShareInformationDaoImpl extends HibernateDaoSupport implements
 										Criteria.DISTINCT_ROOT_ENTITY));
 	}
 
+	/**
+	 * Usado en "Albumes compartidos conmigo" para mostrar la lista de usuarios
+	 * que me comparten algo.
+	 * 
+	 * @param userId
+	 *            Id del usuario con el que se han compartido albumes.
+	 */
 	@SuppressWarnings("unchecked")
 	public ArrayList<ShareInformation> getUserShares(int userId) {
+//		debug("getUserShares(int userId)");
 		return (ArrayList<ShareInformation>) getHibernateTemplate()
 				.findByCriteria(
 						DetachedCriteria
