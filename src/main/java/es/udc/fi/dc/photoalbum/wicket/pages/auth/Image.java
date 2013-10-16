eaa6f76d2304605c5364924cd53b77b7faaa700e
AlejandroFortes <alejandro.fortes.lopes@gmail.com>
2013-10-16 11:18:59 +0200

    RESOLVED - task 153: Arreglar gestion de la privacidad "Heredar privacidad del album" http://git.fic.udc.es/redmine/issues/show/153
    Cambios en la interfaz para  dar soporte a la gestion de la privacidad
    al "Heredar privacidad del album"
    
    WicketApp.utf8.properties:
    WicketApp.utf8.es.properties:
    - Añadidos algunos mensajes wicket para que salgan junto a las nuevas
    funcionalidades 
    
    Image.java
    Image.html
    - Eliminar toda la "Share Information" de cada foto cuando pasa al
    estado "Heredar privacidad del album"
    - Eliminar la posibilidad de añadir el email a una foto cuando el estado
    es "Heredar privacidad del album"
    - Añadir un cuadro de texto para poder asignar a cada foto un mail

--- src/main/java/es/udc/fi/dc/photoalbum/wicket/pages/auth/Image.java
+++ src/main/java/es/udc/fi/dc/photoalbum/wicket/pages/auth/Image.java
@@ -1,26 +1,43 @@
 package es.udc.fi.dc.photoalbum.wicket.pages.auth;
 
 import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.Session;
 import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.image.NonCachingImage;
 import org.apache.wicket.markup.html.image.resource.BlobImageResource;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.navigation.paging.PagingNavigator;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.validation.validator.EmailAddressValidator;
 
 import es.udc.fi.dc.photoalbum.hibernate.Album;
+import es.udc.fi.dc.photoalbum.hibernate.FileShareInformation;
+import es.udc.fi.dc.photoalbum.hibernate.User;
 import es.udc.fi.dc.photoalbum.spring.FileService;
+import es.udc.fi.dc.photoalbum.spring.FileShareInformationService;
+import es.udc.fi.dc.photoalbum.spring.UserService;
 import es.udc.fi.dc.photoalbum.utils.PrivacyLevel;
 import es.udc.fi.dc.photoalbum.wicket.BlobFromFile;
+import es.udc.fi.dc.photoalbum.wicket.MyAjaxButton;
 import es.udc.fi.dc.photoalbum.wicket.MySession;
 import es.udc.fi.dc.photoalbum.wicket.NavigateForm;
 import es.udc.fi.dc.photoalbum.wicket.models.AlbumModel;
@@ -34,10 +51,15 @@ public class Image extends BasePageAuth {
 
 	@SpringBean
 	private FileService fileService;
+	@SpringBean
+	private UserService userService;
+	@SpringBean
+	private FileShareInformationService shareInformationService;
 	private FileOwnModel fileOwnModel;
 	private PageParameters parameters;
 	private Album selectedAlbum;
 	private PrivacyLevelOption selectedPrivacyLevel;
+	private static final int ITEMS_PER_PAGE = 20;
 
 	public Image(final PageParameters parameters) {
 		super(parameters);
@@ -55,17 +77,45 @@ public class Image extends BasePageAuth {
 			this.parameters = parameters;
 			add(new NavigateForm<Void>("formNavigate", am.getObject().getId(),
 					fileOwnModel.getObject().getId(), Image.class));
+			DataView<FileShareInformation> dataView = createShareDataView();
+			add(dataView);
+			add(new PagingNavigator("navigator", dataView));
 			add(createNonCachingImage());
 			add(createFormDelete());
 			add(createFormMove());
 			add(createFormPrivacyLevel());
 			add(new BookmarkablePageLink<Void>("linkBack", Upload.class,
 					(new PageParameters()).add("album", name)));
+			add(createShareForm());
 		} else {
 			throw new RestartResponseException(ErrorPage404.class);
 		}
 	}
 
+	private DataView<FileShareInformation> createShareDataView() {
+		final List<FileShareInformation> list = new ArrayList<FileShareInformation>(
+				shareInformationService.getFileShares(this.fileOwnModel.getObject().getId()));
+		DataView<FileShareInformation> dataView = new DataView<FileShareInformation>(
+				"pageable", new ListDataProvider<FileShareInformation>(list)) {
+
+			protected void populateItem(Item<FileShareInformation> item) {
+				final FileShareInformation shareInformation = item.getModelObject();
+				item.add(new Label("email", shareInformation.getUser().getEmail()));
+				item.add(new Link<Void>("delete") {
+					public void onClick() {
+						shareInformationService.delete(shareInformation);
+						info(new StringResourceModel("share.deleted", this,
+								null).getString());
+						setResponsePage(new Image(parameters));
+					}
+					
+				});	
+			}
+		};
+		dataView.setItemsPerPage(ITEMS_PER_PAGE);
+		return dataView;
+	}
+	
 	private NonCachingImage createNonCachingImage() {
 		return new NonCachingImage("img", new BlobImageResource() {
 			protected Blob getBlob() {
@@ -98,8 +148,8 @@ public class Image extends BasePageAuth {
 			}
 		};
 		DropDownChoice<Album> listAlbums = new DropDownChoice<Album>("albums",
-				new PropertyModel<Album>(this, "selectedAlbum"), new AlbumsModel(
-						fileOwnModel.getObject().getAlbum()),
+				new PropertyModel<Album>(this, "selectedAlbum"),
+				new AlbumsModel(fileOwnModel.getObject().getAlbum()),
 				new ChoiceRenderer<Album>("name", "id"));
 		listAlbums.setRequired(true);
 		listAlbums.setLabel(new StringResourceModel("image.moveAlbum", this,
@@ -113,8 +163,19 @@ public class Image extends BasePageAuth {
 		Form<Void> form = new Form<Void>("formPrivacyLevel") {
 			@Override
 			public void onSubmit() {
-				if (selectedPrivacyLevel != null &&PrivacyLevel.validateFile(selectedPrivacyLevel.getValue())) {
-					fileService.changePrivacyLevel(fileOwnModel.getObject(), selectedPrivacyLevel.getValue());
+				if (selectedPrivacyLevel != null
+						&& PrivacyLevel.validateFile(selectedPrivacyLevel
+								.getValue())) {
+					if (selectedPrivacyLevel.getValue() == PrivacyLevel.INHERIT_FROM_ALBUM) {
+						ArrayList<FileShareInformation> fileShares = shareInformationService
+								.getFileShares(fileOwnModel.getObject().getId());
+						int i;
+						for (i = 0; i < fileShares.size(); i++) {
+							shareInformationService.delete(fileShares.get(i));
+						}
+					}
+					fileService.changePrivacyLevel(fileOwnModel.getObject(),
+							selectedPrivacyLevel.getValue());
 					info(new StringResourceModel("privacyLevel.changed", this,
 							null).getString());
 				}
@@ -125,13 +186,77 @@ public class Image extends BasePageAuth {
 				.getPrivacyLevel(), this);
 		DropDownChoice<PrivacyLevelOption> listPrivacyLevel = new DropDownChoice<PrivacyLevelOption>(
 				"privacyLevels", new PropertyModel<PrivacyLevelOption>(this,
-						"selectedPrivacyLevel"), new PrivacyLevelsModel(fileOwnModel.getObject(),this),
+						"selectedPrivacyLevel"), new PrivacyLevelsModel(
+						fileOwnModel.getObject(), this),
 				new ChoiceRenderer<PrivacyLevelOption>("label", "value"));
 		listPrivacyLevel.setRequired(true);
 		listPrivacyLevel.setLabel(new StringResourceModel(
 				"privacyLevel.change", this, null));
 		form.add(listPrivacyLevel);
-        
+
+		return form;
+	}
+
+	private Form<User> createShareForm() {
+		Form<User> form = new Form<User>("form") {
+			@Override
+			protected void onSubmit() {
+				User user = getModelObject();
+				User existedUser = userService.getUser(user);
+				if (existedUser == null) {
+					error(new StringResourceModel("share.noUser", this, null)
+							.getString());
+				} else if (existedUser.getEmail().equals(
+						userService.getById(
+								((MySession) Session.get()).getuId())
+								.getEmail())) {
+					error(new StringResourceModel("share.yourself", this, null)
+							.getString());
+				} else {
+					FileShareInformation shareInformation = new FileShareInformation(
+							null, fileOwnModel.getObject(), existedUser);
+					ArrayList<FileShareInformation> getFileShares = shareInformationService
+							.getFileShares(fileOwnModel.getObject().getId());
+					ListIterator<FileShareInformation> iter = getFileShares
+							.listIterator();
+					boolean comp = false;
+					while (iter.hasNext() && (comp == false)) {
+						FileShareInformation item = (FileShareInformation) iter
+								.next();
+						String shMail = item.getUser().getEmail();
+						if (shMail == existedUser.getEmail()) {
+							comp = true;
+						}
+					}
+					if (!comp) {
+						shareInformationService.create(shareInformation);
+						info(new StringResourceModel("share.shareFileSuccess",
+								this, null).getString());
+						setResponsePage(new Image(getPage().getPageParameters()));
+					} else {
+						error(new StringResourceModel("share.alreadyExist",
+								this, null).getString());
+					}
+				}
+			}
+		};
+		User user = new User();
+		form.setDefaultModel(new Model<User>(user));
+		RequiredTextField<String> shareEmail = new RequiredTextField<String>(
+				"shareEmail", new PropertyModel<String>(user, "email"));
+		if (fileOwnModel.getObject().getPrivacyLevel().equals(PrivacyLevel.INHERIT_FROM_ALBUM)) {
+			shareEmail.setEnabled(false);
+		} else {
+			shareEmail.setEnabled(true);
+		}
+		shareEmail.setLabel(new StringResourceModel("share.emailField", this,
+				null));
+		shareEmail.add(EmailAddressValidator.getInstance());
+		form.add(shareEmail);
+		FeedbackPanel feedback = new FeedbackPanel("feedback");
+		feedback.setOutputMarkupId(true);
+		form.add(feedback);
+		form.add(new MyAjaxButton("ajax-button", form, feedback));
 		return form;
 	}
 
