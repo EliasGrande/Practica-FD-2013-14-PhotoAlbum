d583a3bd96d5b829ee6b4dbe70ea050b18e108d0
EliasGrande <elias.grande.casedas@gmail.com>
2013-10-31 15:54:28 +0100

    Arreglado cargado de bean LikeAndDislikeService
    -- LikeAndDislikeServiceImpl
    
    Añadidos getter y setter para el dao.
    
    -- applicationContext.xml
    
    Arreglado implementacion de LikeAndDislikeService.

--- src/main/java/es/udc/fi/dc/photoalbum/spring/LikeAndDislikeServiceImpl.java
+++ src/main/java/es/udc/fi/dc/photoalbum/spring/LikeAndDislikeServiceImpl.java
@@ -1,9 +1,20 @@
 package es.udc.fi.dc.photoalbum.spring;
 
 import es.udc.fi.dc.photoalbum.hibernate.LikeAndDislike;
+import es.udc.fi.dc.photoalbum.hibernate.LikeAndDislikeDao;
 import es.udc.fi.dc.photoalbum.hibernate.User;
 
 public class LikeAndDislikeServiceImpl implements LikeAndDislikeService{
+
+	private LikeAndDislikeDao likeAndDislikeDao;
+
+	public LikeAndDislikeDao getLikeAndDislikeDao() {
+		return this.likeAndDislikeDao;
+	}
+
+	public void setLikeAndDislikeDao(LikeAndDislikeDao likeAndDislikeDao) {
+		this.likeAndDislikeDao = likeAndDislikeDao;
+	}
 
 	public void voteLike(LikeAndDislike likeAndDislike, User user) {
 		// TODO Auto-generated method stub
