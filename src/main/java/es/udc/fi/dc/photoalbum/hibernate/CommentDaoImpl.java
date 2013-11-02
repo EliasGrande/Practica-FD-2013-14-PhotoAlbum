4c23204db0af2e310fe0633dafab42d34f13424f
AlejandroFernandezGarcia <alex11_campo@hotmail.com>
2013-11-02 02:12:43 +0100

    RESOLVED - task 303: DAOs y servicios para comentarios y like/dislike http://git.fic.udc.es/redmine/issues/show/303
    Añadidos criterios de ordenación por fecha en los comentarios.

--- src/main/java/es/udc/fi/dc/photoalbum/hibernate/CommentDaoImpl.java
+++ src/main/java/es/udc/fi/dc/photoalbum/hibernate/CommentDaoImpl.java
@@ -18,7 +18,8 @@ public class CommentDaoImpl extends HibernateDaoSupport implements CommentDao {
 	@SuppressWarnings("unchecked")
 	public ArrayList<Comment> getComments(Album album) {
 		String hql = "SELECT c FROM Comment c "
-				+ "WHERE c.album.id = :albumId";
+				+ "WHERE c.album.id = :albumId "
+				+ "ORDER BY c.date DESC";
 
 		return (ArrayList<Comment>) getHibernateTemplate()
 		.getSessionFactory()
@@ -30,7 +31,8 @@ public class CommentDaoImpl extends HibernateDaoSupport implements CommentDao {
 	@SuppressWarnings("unchecked")
 	public ArrayList<Comment> getComments(File file) {
 		String hql = "SELECT c FROM Comment c "
-				+ "WHERE c.file.id = :fileId";
+				+ "WHERE c.file.id = :fileId "
+				+ "ORDER BY c.date DESC";
 
 		return (ArrayList<Comment>) getHibernateTemplate()
 		.getSessionFactory()
@@ -43,7 +45,8 @@ public class CommentDaoImpl extends HibernateDaoSupport implements CommentDao {
 	public ArrayList<Comment> getCommentsPaging(Album album, int first,
 			int count) {
 		String hql = "SELECT c FROM Comment c "
-				+ "WHERE c.album.id = :albumId";
+				+ "WHERE c.album.id = :albumId "
+				+ "ORDER BY c.date DESC";
 
 		return (ArrayList<Comment>) getHibernateTemplate()
 		.getSessionFactory()
@@ -58,7 +61,8 @@ public class CommentDaoImpl extends HibernateDaoSupport implements CommentDao {
 	@SuppressWarnings("unchecked")
 	public ArrayList<Comment> getCommentsPaging(File file, int first, int count) {
 		String hql = "SELECT c FROM Comment c "
-				+ "WHERE c.file.id = :fileId";
+				+ "WHERE c.file.id = :fileId "
+				+ "ORDER BY c.date DESC";
 
 		return (ArrayList<Comment>) getHibernateTemplate()
 		.getSessionFactory()
