2a946d4f14eff10252773a9c19f4c8ca8a757796
EliasGrande <elias.grande.casedas@gmail.com>
2013-11-03 19:21:36 +0100

    Añadido script para hacer un backup del workspace de eclipse.
    Lo añado porque tuve que pasar por el tedioso proceso de reconfigurar
    todo el eclipse de nuevo tras un cuelgue, y posterior reset a lo bruto,
    del ordenador que me corrompió el workspace.
    
    Crea un "$nombre_carpeta_workspace.backup.tar" en la carpeta que
    contiene a la carpeta del workspace, es decir, el workspace y su backup
    quedan al mismo nivel en el sistema de archivos.
    
    Cuidado al usarlo ya que se carga el backup anterior si existe, así que
    no hagáis sin querer un backup un workspace corrupto.

---
+++ src/main/resources/backup-eclipse-workspace.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+script_dir=$(dirname "$0")
+
+echo
+echo "--> Going to script folder..."
+echo "--> [exec] cd \"$script_dir\""
+cd "$script_dir"
+
+echo
+echo "--> Going to project folder..."
+echo "--> [exec] cd ../../../.."
+cd ../../../..
+
+echo
+echo "--> Getting workspace folder name..."
+workspace_dirname=$(pwd | sed 's/.*\///')
+workspace_tar="./$workspace_dirname.backup.tar"
+
+echo
+echo "--> Going to workspace parent folder..."
+echo "--> [exec] cd .."
+cd ..
+
+echo
+echo "--> Removing previous backup..."
+echo "--> [exec] rm -f \"./$workspace_dirname.backup.tar\""
+rm -f "./$workspace_dirname.backup.tar"
+
+echo
+echo "--> Building new backup..."
+echo "--> [exec] tar -cvf \"$workspace_tar\" \"$workspace_dirname\""
+tar -cvf "$workspace_tar" "$workspace_dirname"
+
+echo
+echo "--> Done."
+echo
