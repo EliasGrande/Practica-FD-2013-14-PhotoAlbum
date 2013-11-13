c97e288390911514ef82683027000df6f53596d5
EliasGrande <elias.grande.casedas@gmail.com>
2013-11-13 03:58:29 +0100

    IN PROGRESS - task 506: (Elías) Corregir issues sonar http://git.fic.udc.es/redmine/issues/show/506
    Arreglados issues de sonar referentes a las clases CommentAndVotePanel y
    ShowCommentsPanel.
    
    Eran básicamente asuntos de falta de javadoc, un método muy largo, falta
    de llaves en if/else y repetición de Strings literales en vez de usar
    constantes.

--- src/main/java/es/udc/fi/dc/photoalbum/wicket/panels/CommentAndVotePanel.java
+++ src/main/java/es/udc/fi/dc/photoalbum/wicket/panels/CommentAndVotePanel.java
@@ -5,20 +5,55 @@ import org.apache.wicket.markup.html.panel.Panel;
 import es.udc.fi.dc.photoalbum.hibernate.Album;
 import es.udc.fi.dc.photoalbum.hibernate.File;
 
+/**
+ * Reusable panel for voting an album or file, adding comments to it,
+ * and showing (and voting) comments.
+ */
 @SuppressWarnings("serial")
 public class CommentAndVotePanel extends Panel {
 
+    /**
+     * Inner {@link VotePanel} {@code wicket:id}.
+     */
+    public static final String VOTE_ID = "vote";
+
+    /**
+     * Inner {@link AddCommentPanel} {@code wicket:id}.
+     */
+    public static final String ADD_COMMENT_ID = "addComment";
+
+    /**
+     * Inner {@link ShowCommentsPanel} {@code wicket:id}.
+     */
+    public static final String SHOW_COMMENTS_ID = "showComments";
+
+    /**
+     * Defines a {@link CommentAndVotePanel} object of an {@link Album}.
+     * 
+     * @param id
+     *            CommentAndVotePanel {@code wicket:id}
+     * @param album
+     *            Album object
+     */
     public CommentAndVotePanel(String id, Album album) {
         super(id);
-        add(new VotePanel("vote", album.getLikeAndDislike()));
-        add(new AddCommentPanel("addComment", album));
-        add(new ShowCommentsPanel("showComments", album));
+        add(new VotePanel(VOTE_ID, album.getLikeAndDislike()));
+        add(new AddCommentPanel(ADD_COMMENT_ID, album));
+        add(new ShowCommentsPanel(SHOW_COMMENTS_ID, album));
     }
-
+    
+    /**
+     * Defines a {@link CommentAndVotePanel} object of a {@link File}.
+     * 
+     * @param id
+     *            CommentAndVotePanel {@code wicket:id}
+     * @param file
+     *            File object
+     */
     public CommentAndVotePanel(String id, File file) {
         super(id);
-        add(new VotePanel("vote", file.getLikeAndDislike()));
-        add(new AddCommentPanel("addComment", file));
-        add(new ShowCommentsPanel("showComments", file));
+        add(new VotePanel(VOTE_ID, file.getLikeAndDislike()));
+        add(new AddCommentPanel(ADD_COMMENT_ID, file));
+        add(new ShowCommentsPanel(SHOW_COMMENTS_ID, file));
     }
 }
--- src/main/java/es/udc/fi/dc/photoalbum/wicket/panels/ShowCommentsPanel.java
+++ src/main/java/es/udc/fi/dc/photoalbum/wicket/panels/ShowCommentsPanel.java
@@ -10,30 +10,115 @@ import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.Session;
-import es.udc.fi.dc.photoalbum.wicket.MySession;
 
+import es.udc.fi.dc.photoalbum.wicket.MySession;
 import es.udc.fi.dc.photoalbum.hibernate.Album;
 import es.udc.fi.dc.photoalbum.hibernate.Comment;
 import es.udc.fi.dc.photoalbum.hibernate.File;
 import es.udc.fi.dc.photoalbum.utils.TimeAgoCalendarFormat;
 import es.udc.fi.dc.photoalbum.wicket.models.CommentsModel;
 
+/**
+ * Reusable panel for showing and voting the comments of an album or
+ * file.
+ */
 @SuppressWarnings("serial")
 public class ShowCommentsPanel extends Panel {
 
-    private CommentsModel commentsModel;
-    private TimeAgoCalendarFormat calendarFormat;
+    /**
+     * {@link #commentContainer} {@code wicket:id}.
+     */
+    public static final String CONTAINER_ID = "commentContainer";
 
+    /**
+     * Comment author {@code wicket:id}.
+     */
+    public static final String USER_LABEL_ID = "user";
+
+    /**
+     * Comment date {@code wicket:id}.
+     */
+    public static final String DATE_LABEL_ID = "date";
+
+    /**
+     * Comment text {@code wicket:id}.
+     */
+    public static final String TEXT_LABEL_ID = "text";
+
+    /**
+     * Comment {@link VotePanel} {@code wicket:id}.
+     */
+    public static final String VOTE_LABEL_ID = "vote";
+
+    /**
+     * More-link {@code wicket:id}.
+     */
+    public static final String MORE_LINK_ID = "moreLink";
+
+    /**
+     * Class attribute of the more-link when there is no more
+     * comments.
+     */
+    public static final String NO_MORE_COMMENTS_LINK_CLASS = "hidden";
+
+    /**
+     * Number of comments requested when the page is rendered and
+     * every time the user press the more-link.
+     */
     private static final int COMMENTS_PER_PANEL = 10;
 
+    /**
+     * Stores the list of comments and votes.
+     */
+    private CommentsModel commentsModel;
+
+    /**
+     * Comment date format.
+     */
+    private TimeAgoCalendarFormat calendarFormat;
+
+    /**
+     * Container for the comments-view and more-link refreshable by
+     * ajax using the more-link.
+     */
+    private WebMarkupContainer commentContainer;
+
+    /**
+     * Defines a {@link ShowCommentsPanel} object of an {@link Album}.
+     * 
+     * @param id
+     *            ShowCommentsPanel {@code wicket:id}
+     * @param album
+     *            Album object
+     */
     public ShowCommentsPanel(String id, Album album) {
         this(id, album, null);
     }
 
+    /**
+     * Defines a {@link ShowCommentsPanel} object of a {@link File}.
+     * 
+     * @param id
+     *            ShowCommentsPanel {@code wicket:id}
+     * @param file
+     *            File object
+     */
     public ShowCommentsPanel(String id, File file) {
         this(id, null, file);
     }
 
+    /**
+     * Defines a {@link ShowCommentsPanel} object of an {@link Album}
+     * or {@link File}, either the album or the file must be null, but
+     * not both.
+     * 
+     * @param id
+     *            ShowCommentsPanel {@code wicket:id}
+     * @param album
+     *            Album object, or null for file comments
+     * @param file
+     *            File object, or null for album comments
+     */
     private ShowCommentsPanel(String id, Album album, File file) {
         super(id);
         int userId = ((MySession) Session.get()).getuId();
@@ -41,30 +126,47 @@ public class ShowCommentsPanel extends Panel {
                 COMMENTS_PER_PANEL, userId);
         calendarFormat = new TimeAgoCalendarFormat(this);
 
-        // ajax data container
-        final WebMarkupContainer commentContainer = new WebMarkupContainer(
-                "commentContainer");
+        makeAjaxContainer();
+        makeCommentsView();
+        makeMoreLink();
+    }
+
+    /**
+     * Initializes {@link #commentContainer} and adds it to the panel.
+     */
+    private void makeAjaxContainer() {
+        commentContainer = new WebMarkupContainer(CONTAINER_ID);
         commentContainer.setOutputMarkupId(true);
         add(commentContainer);
+    }
 
+    /**
+     * Creates the comments-view and adds it to the {@link #commentContainer}.
+     */
+    private void makeCommentsView() {
         ListView<Comment> commentsView = new ListView<Comment>(
                 "comment", commentsModel) {
             @Override
             protected void populateItem(ListItem<Comment> item) {
                 Comment comment = item.getModelObject();
-                item.add(new Label("user", comment.getUser()
+                item.add(new Label(USER_LABEL_ID, comment.getUser()
                         .getEmail()));
-                item.add(new Label("date", calendarFormat
+                item.add(new Label(DATE_LABEL_ID, calendarFormat
                         .format(comment.getDate())));
-                item.add(new Label("text", comment.getText()));
-                item.add(new VotePanel("vote", comment
+                item.add(new Label(TEXT_LABEL_ID, comment.getText()));
+                item.add(new VotePanel(VOTE_LABEL_ID, comment
                         .getLikeAndDislike(), commentsModel
                         .getVoted(comment)));
             }
         };
         commentContainer.add(commentsView);
+    }
 
-        AjaxLink<String> moreLink = new AjaxLink<String>("moreLink") {
+    /**
+     * Creates the more-link and adds it to the {@link #commentContainer}.
+     */
+    private void makeMoreLink() {
+        AjaxLink<String> moreLink = new AjaxLink<String>(MORE_LINK_ID) {
             public void onClick(AjaxRequestTarget target) {
                 target.add(commentContainer);
                 // avoid the annoying scrolling-down
@@ -74,8 +176,9 @@ public class ShowCommentsPanel extends Panel {
             @Override
             protected void onComponentTag(ComponentTag tag) {
                 super.onComponentTag(tag);
-                if (!commentsModel.hasMore())
-                    tag.put("class", "hidden");
+                if (!commentsModel.hasMore()) {
+                    tag.put("class", NO_MORE_COMMENTS_LINK_CLASS);
+                }
             }
 
             @Override
@@ -84,7 +187,6 @@ public class ShowCommentsPanel extends Panel {
             }
         };
         commentContainer.add(moreLink);
-
     }
 
     @Override
